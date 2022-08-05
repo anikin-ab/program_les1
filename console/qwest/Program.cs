f61cccfc7799431b46c6798480442ae427f10cd7
Anikin A.B <abanikin87@gmail.com>
2022-08-05 20:32:16 +0400

    qwest

---
+++ console/qwest/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int maxnum = a;
+
+if (a > maxnum) maxnum = a;
+if (b > maxnum) maxnum = b;
+if (c > maxnum) maxnum = c;
+if (d > maxnum) maxnum = d;
+if (e > maxnum) maxnum = e;
+if (f > maxnum) maxnum = f;
+
+Console.Write("maxnum = ");
+Console.WriteLine(maxnum);
