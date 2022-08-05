d776d27e55d624df7b6f7979791540e01dad6afe
Anikin A.B <abanikin87@gmail.com>
2022-08-05 20:23:05 +0400

    cycle

---
+++ console/cycle/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("input user name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "alex")
+{
+    Console.WriteLine("hi bro");
+}
+else
+{
+    Console.Write("hello newbie ");
+    Console.Write(username);
+}
